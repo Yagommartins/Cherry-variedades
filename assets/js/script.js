a14250ecc6c04303fc751658a602c7cbb83e4b29
Yagommartins <yagomartins64@gmail.com>
2025-04-29 17:24:12 -0300

    Parte de busca

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,4 +7,42 @@ document.addEventListener('DOMContentLoaded', function () {
 
         })
     })
+
+    //fechar o dropdown ao clicar fora
+
+    document.addEventListener('click', function (e) {
+        dropdownToggle.forEach((toggle) => {
+            const parent = toggle.closest('.dropdown-item');
+            if (!parent.contains(e.target)) {
+                parent.classList.remove('active');
+            }
+
+        })
+    })
+
+})
+
+const searchToggle = document.getElementById('searchToggle');
+const searchOverlay = document.getElementById('searchOverlay');
+const closeSearch = document.getElementById('closeSearch');
+
+searchToggle.addEventListener('click', () => {
+    searchOverlay.style.display = 'flex';
+    serTimeout(() => {
+        searchOverlay.querySelector('.search-input').focus();
+    }, 100);
+})
+
+//fechar o botão de pesquisa
+
+closeSearch.addEventListener('click', () => {
+    searchOverlay.style.display = 'none';
+})
+
+//fechar com o esc
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        searchOverlay.style.display = 'none';
+    }
 })
